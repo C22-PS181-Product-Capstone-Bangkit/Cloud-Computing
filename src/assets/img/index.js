f94a2edf65c4019bc488c3c2c84609b273628bcd
Patrix2001 <juanpatrick2001@gmail.com>
2022-05-19 16:10:10 +0700

    rename images

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,8 +1,8 @@
 import Phone from "./phone.svg";
-import Screen1 from "./Welcome Screen.svg";
-import Screen2 from "./Search Result.svg";
-import Screen3 from "./Detail Restaurant.svg";
+import Screen1 from "./WelcomeScreen.svg";
+import Screen2 from "./SearchResult.svg";
+import Screen3 from "./DetailRestaurant.svg";
 import Screen4 from "./History.svg";
-import Screen5 from "./Recommend Restaurant.svg";
+import Screen5 from "./RecommendRestaurant.svg";
 
 export { Phone, Screen1, Screen2, Screen3, Screen4, Screen5  }
