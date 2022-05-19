eb79e0778550487e7d19be4fd9c9c5d6eda48194
Patrix2001 <juanpatrick2001@gmail.com>
2022-05-19 10:16:45 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="md:grid-cols-4 grid-cols-2 grid bg-gray-900 pt-5 text-gray-400 min-h-full gap-8 mt-16">
-      <section className="font-semibold">
-        <p>Alpha</p>
+    <footer className="md:grid-cols-4 grid-cols-2 grid bg-gray-900 pt-5 gap-3 text-gray-400 min-h-full mt-16 font-semibold">
+      <section className="mx-6 text-right">
+        <p>Logo</p>
       </section>
-      <section className="font-semibold">
-        <p>Alpha</p>
+      <section className="mx-6">
+        <p>Cemil adalah aplikasi rekomendasi makanan</p>
       </section>
-      <section className="font-semibold">
-        <p>Alpha</p>
+      <section className="mx-6">
       </section>
-      <section className="font-semibold">
-        <p>Alpha</p>
+      <section className="text-center mx-6">
       </section>
-      <section className="md:col-span-4 col-span-2 bg-gray-800 text-center py-2 mt-auto">
+      <section className="md:col-span-4 col-span-2 bg-gray-800 text-center py-2 mt-12">
         <p className="text-xs">© Copyright 2022 Cemil Choose Your Meal</p>
       </section>
     </footer>
